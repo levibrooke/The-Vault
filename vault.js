2cbae7b886e11f9d86db6772496b0584237b6ff6
levi <levi@levibrooke.com>
2018-01-02 18:49:21 -1000

    Complete exercise

--- vault.js
+++ vault.js
@@ -1,4 +1,23 @@
 'use strict';
 module.exports = function() {
   
+  let vault = {};
+
+  function setValue(key, value) {
+    vault[key] = value;
+  };
+  
+  function getValue(key) {
+    if (key === undefined || !vault.hasOwnProperty(key) ) {
+      return null;
+    } else {
+      return vault[key];
+    }
+  };
+
+  return {
+    setValue: setValue,
+    getValue: getValue
+  }
+
 };
